d337e18277c6cd91d1f422fa100cd0c2353349b8
Berke <berkesat.dev@gmail.com>
2024-03-18 18:32:16 +0300

    First commit

--- src/Area.java
+++ src/Area.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Area {
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        int firstSide, secondSide, thirdSide;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter first side length: ");
+        firstSide = input.nextInt();
+        System.out.print("Please enter second side length: ");
+        secondSide = input.nextInt();
+        System.out.print("Please enter third side length: ");
+        thirdSide = input.nextInt();
+
+        int perimeter = firstSide + secondSide + thirdSide;
+        int u = perimeter / 2;
+        double area = Math.sqrt(u * (u - firstSide) * (u - secondSide) * (u - thirdSide));
+        System.out.println("This triangle area is " + area);
+
+
+    }
 }
